b6d2f45a403002fcae4fb8fbd47b1f5a56ff4491
Derek Townsend <derekjavon2@gmail.com>
2019-10-28 14:02:26 -0500

    Updates

---
+++ precache-manifest.00a81f7f2742cfd2a63776ba14619936.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a970abbc2605312e7aa80968fd809de4",
+    "url": "/bertel-co/index.html"
+  },
+  {
+    "revision": "1239e286d54dbc78cbff",
+    "url": "/bertel-co/static/css/main.36b90be2.chunk.css"
+  },
+  {
+    "revision": "eb260e586174c3bdd55e",
+    "url": "/bertel-co/static/js/2.b1599b6c.chunk.js"
+  },
+  {
+    "revision": "1239e286d54dbc78cbff",
+    "url": "/bertel-co/static/js/main.5407ee20.chunk.js"
+  },
+  {
+    "revision": "036f5d221ac50cd9323f",
+    "url": "/bertel-co/static/js/runtime-main.7eb0d175.js"
+  },
+  {
+    "revision": "1774133f7e7cebf3c6073674429f4af0",
+    "url": "/bertel-co/static/media/BPlogo(modified).1774133f.png"
+  },
+  {
+    "revision": "b23a36db44791884cfa98aa28cc86f37",
+    "url": "/bertel-co/static/media/BPlogo.b23a36db.jpg"
+  },
+  {
+    "revision": "d2139813200f5c35a6468adce99311b3",
+    "url": "/bertel-co/static/media/beams.d2139813.png"
+  },
+  {
+    "revision": "91e405a7c32cfde1be56f8a357bb7ea3",
+    "url": "/bertel-co/static/media/bus-statation.91e405a7.JPEG"
+  },
+  {
+    "revision": "47199618f2bec49c426110287a38f22b",
+    "url": "/bertel-co/static/media/divider.47199618.JPG"
+  },
+  {
+    "revision": "18145915689d9dc19459a571d9569364",
+    "url": "/bertel-co/static/media/hero2.18145915.jpeg"
+  },
+  {
+    "revision": "76e048442c3c1b3c81995b41bf191822",
+    "url": "/bertel-co/static/media/lanterns.76e04844.png"
+  },
+  {
+    "revision": "d54568aba966a2c4ccf4735239522c96",
+    "url": "/bertel-co/static/media/office.d54568ab.png"
+  },
+  {
+    "revision": "20c5f844715b4f2abe2786c93f8092c9",
+    "url": "/bertel-co/static/media/office2.20c5f844.png"
+  }
+]);
